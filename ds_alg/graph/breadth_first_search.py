1461455576c5cf6e71b0c07ce05d94cbeb8b3200
mcshakes <efmccarthy26@gmail.com>
2020-09-03 15:26:59 -0600

    initial

--- ds_alg/graph/breadth_first_search.py
+++ ds_alg/graph/breadth_first_search.py
@@ -2,3 +2,12 @@ from graph import Graph
 from vertex import Vertex
 from queue.queue import Queue
 
+
+def bfs(g, start):
+    start.set_distance(0)
+    start.set_pred(None)
+    vert_queue = Queue()
+    vert_queue.enqueue(start)
+
+    while vert_queue.size() > 0:
+        current_vert = vert_queue.dequeue()
